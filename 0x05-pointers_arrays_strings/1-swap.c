52a791c453b173043e9448934d58614a798247f8
Pelice124 <felixasiedukofi@gmail.com>
2023-03-27 17:56:59 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of int a and int b
+* By Pelice
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
+		int z;
+
+		z = *a;
+		*a = *b;
+		*b = z;
+}
+

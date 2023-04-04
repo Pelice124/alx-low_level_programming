634ea5901359e1b927e4f032b17ba931d69a5f18
Pelice124 <felixasiedukofi@gmail.com>
2023-04-03 20:21:34 -0700

    task 6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+* This header file has all prototypes for the 0x    07. C- even more pointers
+* _putchar - function
+* @c: parameter
+* Return: on success 1, on error -1
+*/
+
+
+
+
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);

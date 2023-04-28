8dd0250424ce934b3d7d0ca95e730882ea87d733
Pelice124 <felixasiedukofi@gmail.com>
2023-04-27 21:02:00 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,29 +1,20 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	int count = 0;
+	size_t n = 0;
 
 	while (h)
+
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
+		n++;
 		h = h->next;
 	}
-	return (count);
+	return (n);
 }
